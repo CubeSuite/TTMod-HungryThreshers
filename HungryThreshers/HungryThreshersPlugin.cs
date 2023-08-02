76cfa5ce674ec78d6cb7bdafe9fcbfa72dd007d8
CubeSuite <cubesuite@outlook.com>
2023-08-02 19:32:08 +0100

    Initial commit

---
+++ HungryThreshers/HungryThreshersPlugin.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using HungryThreshers.Patches;
+using UnityEngine;
+
+namespace HungryThreshers
+{
+    // TODO Review this file and update to your own requirements.
+
+    [BepInPlugin(MyGUID, PluginName, VersionString)]
+    public class HungryThreshersPlugin : BaseUnityPlugin
+    {
+        private const string MyGUID = "com.equinox.HungryThreshers";
+        private const string PluginName = "HungryThreshers";
+        private const string VersionString = "1.1.0";
+
+        private const string PauseIfBothFullKey = "PauseIfBothFull";
+        public static ConfigEntry<bool> PauseIfBothFull;
+
+        private static readonly Harmony Harmony = new Harmony(MyGUID);
+        public static ManualLogSource Log = new ManualLogSource(PluginName);
+
+        private void Awake() {
+            Logger.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loading...");
+            PauseIfBothFull = Config.Bind("General", PauseIfBothFullKey, false, new ConfigDescription("When enabled, threshers will not void items if both outputs are full"));
+            
+            Harmony.PatchAll();
+            Log = Logger;
+            Harmony.PatchAll(typeof(ThresherInstancePatch));
+            Logger.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loaded.");
+        }
+    }
+}
---
+++ HungryThreshers/Patches/ThresherInstancePatch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+
+namespace HungryThreshers.Patches
+{
+    public class ThresherInstancePatch
+    {
+        [HarmonyPatch(typeof(ThresherInstance), "UpdateCrafting")]
+        [HarmonyPrefix]
+        private static void clearOutputsForCrafting(ThresherInstance __instance) {
+            Inventory outputInventory = __instance.GetOutputInventory();
+            if (!outputInventory.myStacks[0].isEmpty && !outputInventory.myStacks[1].isEmpty) {
+                if (outputInventory.myStacks[0].count >= 490 && outputInventory.myStacks[1].count >= 490) {
+                    if (HungryThreshersPlugin.PauseIfBothFull.Value) {
+                        return;
+                    }
+                }
+            }
+
+            for(int i = 0; i < outputInventory.myStacks.Count(); i++) {
+                if (!outputInventory.myStacks[i].isEmpty && outputInventory.myStacks[i].count >= 490) {
+                    outputInventory.RemoveResourcesFromSlot(i, 10);
+                }
+            }
+        }
+    }
+}
